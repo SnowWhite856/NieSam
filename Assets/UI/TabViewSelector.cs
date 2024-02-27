60187f3491997200481190b204986336e880ddfe
SnowWhite856 <69056017+SnowWhite856@users.noreply.github.com>
2024-02-27 23:03:03 +0100

    UI update
    -Intial GeneralUI
    -EqUI

---
+++ Assets/UI/TabViewSelector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TabViewSelector : MonoBehaviour
+{
+    private UIDocument Inv;
+    private TabView LeftPanelConttroler;
+
+    private void Start()
+    {
+    }
+}
