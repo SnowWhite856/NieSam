7f1e58172c3bcdd44ce53ff46182abd9c70a0ce0
SnowWhite856 <oliwer.cz123@gmail.com>
2024-03-01 00:44:18 +0100

    InvUI update
    LeftPanel changes (in progress)

--- Assets/UI/TabViewSelector.cs
+++ Assets/UI/TabViewSelector.cs
@@ -1,14 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
 
 public class TabViewSelector : MonoBehaviour
 {
-    private UIDocument Inv;
-    private TabView LeftPanelConttroler;
+    private VisualElement Inv;
+    public VisualTreeAsset[] UIs;
+
+    private VisualElement LeftPanelControler;
+    private VisualElement ChangePanel;
+    
+    private void ButtonHandler()
+    {
+       for(int i = 0; i < Inv.Q<VisualElement>("ChangePanel").childCount; i++)
+       {
+            int index = i;
+            Button button = (Button)Inv.Q<VisualElement>("ChangePanel").ElementAt(i);
+            button.clickable.clicked += () =>
+            {
+                Inv.Q<TabView>("LeftPanel").tabIndex = index;
+            };
+       };
+    }
 
     private void Start()
     {
+        Inv = GetComponent<UIDocument>().rootVisualElement;   
     }
 }
